bcf3fd6c59d80978da9339195e6f9e19947da985
Andrew Kaskaniotis <akaskaniotis@gmail.com>
2021-10-16 13:58:23 +1100

    Repo and interfaces

---
+++ Interface/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using _31927_TextEditor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31927_TextEditor.Interface
+{
+    public interface IUserRepository
+    {
+        void CreateUser(UserModel userModel);
+        bool CheckPermission(UserModel userModel);
+        UserModel GetUser(string ID);
+        UserModel UpdateUser(UserModel userModel);
+
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using _31927_TextEditor.Interface;
+using _31927_TextEditor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31927_TextEditor.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public void CreateUser(UserModel userModel) { 
+
+        }
+
+        public bool CheckPermission(UserModel userModel) {
+
+            return false;
+        }
+
+        public UserModel GetUser(string ID) {
+
+            return null;
+        }
+
+        public UserModel UpdateUser(UserModel userModel) {
+            return null;
+        }
+
+    }
+}
